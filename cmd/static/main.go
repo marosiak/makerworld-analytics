93a4fd34455bfa27149b682b8addef847c64e924
MRosiak <maciejrosiak@protonmail.com>
2025-04-02 21:00:35 +0200

    test deploy

---
+++ cmd/static/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/maxence-charriere/go-app/v10/pkg/app"
+	"log"
+	"makerworld-analytics/ui/views"
+)
+
+func main() {
+	// TODO fix DRY
+	app.Route("/", func() app.Composer {
+		return &views.MainView{}
+	})
+
+	app.RunWhenOnBrowser()
+
+	err := app.GenerateStaticWebsite(".", &app.Handler{
+		Name:        "Makerworld Analytics",
+		Description: "Extended stats for makerworld",
+		Scripts:     []string{"https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"},
+		Styles:      []string{"https://cdn.jsdelivr.net/npm/daisyui@5"},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
